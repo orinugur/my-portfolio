d68c94e5b7ce835ce925e496da29284bd06c15f5
MS <jad@clevi.ai>
2025-07-14 15:19:07 +0900

    qwer
    afsd

--- 1/vite.config.js
+++ 1/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio123/1/', // ← 반드시 실제 배포 경로와 일치!
+  base: '/my-portfolio123/1/dist', // ← 반드시 실제 배포 경로와 일치!
 });
