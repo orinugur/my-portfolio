2386540ea7054e75a0e2b9c5a58c73f69f7f463b
MS <jad@clevi.ai>
2025-07-17 17:54:50 +0900

    Add projects/team175594 project

---
+++ projects/team175594/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-portfolio123/1/', // ← 반드시 실제 배포 경로와 일치!
+});
